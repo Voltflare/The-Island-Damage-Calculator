e46238e4749a5f61850705290d37c66715e0df3b
Nathan Hites <41966910+Voltflare@users.noreply.github.com>
2026-03-26 23:45:10 -0500

    Update items.js

--- calc/data/items.js
+++ calc/data/items.js
@@ -364,7 +364,7 @@ var SS = SM.concat([
     'Pecha Scarf',
     'Persim Band',
     'Pragmatic Sash',
-    "Pyrea's Feather",
+    'Pyreas Feather',
     'Rainy Ribbon',
     'Rebound Band',
     'Return Band',
